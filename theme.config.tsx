77bd3254efcc660cd5d93875a2fcd74a6f7e3baf
bronyowl <a18706028109@outlook.com>
2024-11-03 02:40:58 +0800

    modify

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,19 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>周熠琳女士专用EJU知识点梳理网站</span>,
+  logo: (
+    <span style={{ display: 'inline-flex', alignItems: 'center', gap: '8px' }}>
+      <img
+        src="https://cdn.jsdelivr.net/gh/bronyowl/typopicbed@main/img/RemiFumoLook.png"
+        style={{
+          height: '3em',
+          width: '3em',
+          objectFit: 'contain'
+        }}
+      />
+      周熠琳女士专用EJU知识点梳理网站
+    </span>
+  ),
   project: {
     link: 'https://github.com/bronyowl/documentation-starter-kit',
   },
