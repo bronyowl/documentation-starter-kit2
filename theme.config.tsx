f77c6f9a13199638b23d4dce0fb99128427ce0e9
bronyowl <a18706028109@outlook.com>
2024-11-03 03:19:43 +0800

    modify

--- theme.config.tsx
+++ theme.config.tsx
@@ -16,14 +16,14 @@ const config: DocsThemeConfig = {
     </span>
   ),
   project: {
-    link: 'https://github.com/bronyowl/documentation-starter-kit',
+    link: 'https://github.com/bronyowl/documentation-starter-kit2.git',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/bronyowl/documentation-starter-kit',
+  docsRepositoryBase: 'https://github.com/bronyowl/documentation-starter-kit2.git',
   footer: {
-    text: 'Nextra Docs Template',
+    text: '两眼一睁，开始竞争；提高一分，干掉千人。',
   },
 }
 
